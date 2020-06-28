8cb224730d4887c0318c204e52e713aad2994b1d
syawal2kalii <syawal20162016@gmail.com>
2020-06-28 14:53:28 +0800

    8_metatag_dinamis

--- components/BaseLayout.js
+++ components/BaseLayout.js
@@ -1,6 +1,13 @@
 import Navigation from '../components/Navigation'
+import Head from 'next/head'    
 const Layout = (props) => (
     <div>
+        <Head>
+            <title>Next JS Basic</title>
+            <meta name="description" content=""/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <meta charSet="utf-8" />
+        </Head>
         <Navigation />
         {props.children}
         <p>Footer: copyright 2019</p>
--- pages/single.js
+++ pages/single.js
@@ -1,8 +1,12 @@
 import {withRouter} from 'next/router'
 import Layout from '../components/BaseLayout'
+import Head from 'next/head'
 
 const Page = withRouter((props)=>(
     <Layout>
+        <Head>
+            <title>halaman User</title>
+        </Head>
         <h2>{props.router.query.title}</h2>
         <p>Ini Konten Palsu</p>
     </Layout>
